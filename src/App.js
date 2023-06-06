b2618869694891f2ab21a771c735bdc536ae5ea4
ARod88 <alma.rodriguez72@yahoo.com>
2023-06-05 20:36:29 -0700

    added with suspense

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import {useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
+import React, { useEffect, useState, Suspense } from 'react';
+import Gallery from './components/Gallery';
+import SearchBar from './components/SearchBar';
+import { DataContext } from './context/DataContext';
+import { createResource } from './helper';
+import Spinner from './components/Spinner';
 
 function App() {
-	let [search, setSearch] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
+  const [search, setSearch] = useState('');
+  const [message, setMessage] = useState('Search for Music!');
+  const [data, setData] = useState(null);
 
-	const API_URL = 'https://itunes.apple.com/search?term='
+  const API_URL = 'https://itunes.apple.com/search?term=';
 
-	useEffect(() => {
-		if(search) {
-			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					setData(resData.results)
-				} else {
-					setMessage('Not Found')
-				}
-			}
-			fetchData()
-		}
-	}, [search])
-	
-	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
-	}
+  useEffect(() => {
+    if (search) {
+      fetchData(search);
+    }
+  }, [search]);
 
-	return (
-		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
-		</div>
-  	);
+  const fetchData = async (searchTerm) => {
+    try {
+      const response = await fetch(API_URL + searchTerm);
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  };
+
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term);
+  };
+
+  const renderGallery = () => {
+    if (data) {
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      );
+    }
+  };
+
+  return (
+    <div className="App">
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      {renderGallery()}
+    </div>
+  );
 }
 
 export default App;
